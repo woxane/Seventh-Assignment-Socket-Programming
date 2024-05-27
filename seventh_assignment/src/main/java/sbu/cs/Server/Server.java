8de1b9b094f9071dee5e666241db2cbaf0d3eac1
woxane <mohsen.minavand1384@gmail.com>
2024-05-27 13:35:50 +0330

    Implement Server class

---
+++ seventh_assignment/src/main/java/sbu/cs/Server/Server.java
@@ -0,0 +1,59 @@
+package sbu.cs.Server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Server {
+    private static final int PORT = 12345;
+    private static final int HISTORY_SIZE = 10;
+    private static List<String> chatHistory = new ArrayList<>();
+    private ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public void startServer() {
+        try {
+            while (!serverSocket.isClosed()) {
+                Socket socket = serverSocket.accept();
+                System.out.println("A new client has connected !");
+
+                ClientHandler clientHandler = new ClientHandler(socket);
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+
+            }
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void closeServer() {
+        try {
+            if (serverSocket != null) {
+                serverSocket.close();
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        System.out.println("Server started on port : " + PORT);
+
+        Server server = new Server(serverSocket);
+        server.startServer();
+
+    }
+}
