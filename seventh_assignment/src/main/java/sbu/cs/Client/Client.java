7adf9bcabb0daf41bdc85a89be2c567c760816fd
woxane <mohsen.minavand1384@gmail.com>
2024-05-28 10:45:18 +0330

    minor changes

--- seventh_assignment/src/main/java/sbu/cs/Client/Client.java
+++ seventh_assignment/src/main/java/sbu/cs/Client/Client.java
@@ -84,7 +84,7 @@ public class Client {
 
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your username : ");
         String username = scanner.nextLine();
