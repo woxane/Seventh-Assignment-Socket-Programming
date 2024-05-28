ae89fbc825f67b7375c961d100409156b3fdbbff
woxane <mohsen.minavand1384@gmail.com>
2024-05-28 10:44:58 +0330

    Implement Client class and complete task 1

--- seventh_assignment/src/main/java/sbu/cs/Client/Client.java
+++ seventh_assignment/src/main/java/sbu/cs/Client/Client.java
@@ -1,12 +1,97 @@
 package sbu.cs.Client;
 
-// Client Class
-public class Client {
-    // TODO: Implement the client-side operations
 
-    // TODO: Add constructor and necessary methods
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    private Socket socket;
+    private BufferedWriter bufferedWriter;
+    private BufferedReader bufferedReader;
+    private String username;
+    private static final int PORT = 12345;
+
+
+    public Client(Socket socket , String username) {
+        try {
+            this.socket = socket;
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.username = username;
+
+        } catch (IOException e) {
+            closeEverything(socket , bufferedReader , bufferedWriter);
+        }
+
+    }
+
+
+    public void sendMessage() {
+        try {
+            bufferedWriter.write(this.username);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+
+            Scanner scanner = new Scanner(System.in);
+            while (socket.isConnected()) {
+                String message = scanner.nextLine();
+                bufferedWriter.write(username + " : " + message);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+
+            }
+
+        } catch (IOException e) {
+            closeEverything(socket , bufferedReader , bufferedWriter);
+        }
+    }
+
+
+    public void listenMessage() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String message;
+
+                while (socket.isConnected()) {
+                    try {
+                        message = bufferedReader.readLine();
+                        System.out.println(message);
+                    } catch (IOException e) {
+                        closeEverything(socket , bufferedReader , bufferedWriter);
+                    }
+                }
+            }
+        }).start();
+    }
+
+    public void closeEverything(Socket socket , BufferedReader bufferedReader , BufferedWriter bufferedWriter) {
+        try {
+            if (socket != null) {
+                socket.close();
+            }
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+            if (bufferedWriter != null) {
+                bufferedWriter.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
     public static void main(String[] args) {
-        // TODO: Implement the main method to start the client
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your username : ");
+        String username = scanner.nextLine();
+        Socket socket = new Socket("localhost" , PORT);
+        Client client = new Client(socket , username);
+        client.listenMessage();
+        client.sendMessage();
+
     }
 }
