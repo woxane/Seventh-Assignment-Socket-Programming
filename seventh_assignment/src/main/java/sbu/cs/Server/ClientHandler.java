37c6d950e3978e518b807724941d8f63635935b2
woxane <mohsen.minavand1384@gmail.com>
2024-05-29 15:38:09 +0330

    Write down the schematic of new version of ClientHandler

--- seventh_assignment/src/main/java/sbu/cs/Server/ClientHandler.java
+++ seventh_assignment/src/main/java/sbu/cs/Server/ClientHandler.java
@@ -19,97 +19,31 @@ public class ClientHandler implements Runnable {
             this.socket = socket;
             this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            this.clientUsername = bufferedReader.readLine();
             clientHandlers.add(this);
 
-            historyBroadcast();
-
-            broadcast("SERVER : " + this.clientUsername + " has entered the chat !");
 
         } catch (IOException e) {
             closeEverything(socket , bufferedReader , bufferedWriter);
         }
+
     }
+
 
     @Override
     public void run() {
-        String message;
+        sendMenu();
+        int index = getSelectedOption();
 
-        while (socket.isConnected()) {
-            try {
-                message = bufferedReader.readLine();
-                broadcast(message);
-
-            } catch (IOException e) {
-                closeEverything(socket , bufferedReader , bufferedWriter);
+        switch (index) {
+            // Enter group chat
+            case 1 :
+                enterGroupchat();
                 break;
-            }
+
+            // Download file
+            case 2 :
+                downloadFile();
+                break;
         }
-    }
-
-    public void broadcast(String message) {
-        updateChatHistory(message);
-
-        for (ClientHandler clientHandler : clientHandlers) {
-            try {
-                if (!clientHandler.clientUsername.equals(clientUsername)) {
-                    clientHandler.bufferedWriter.write(message);
-                    clientHandler.bufferedWriter.newLine();
-                    clientHandler.bufferedWriter.flush();
-                }
-            } catch (IOException e) {
-                closeEverything(socket , bufferedReader , bufferedWriter);
-            }
-        }
-    }
-
-    public void removeClientHandler() {
-        clientHandlers.remove(this);
-        broadcast("SERVER : " + this.clientUsername + " has left the chat !");
-    }
-
-    public void updateChatHistory(String message) {
-        chatHistory.add(message);
-        if (chatHistory.size() > HISTORY_SIZE) {
-            chatHistory.remove(0);
-        }
-    }
-
-    public void historyBroadcast() {
-        try {
-            if (chatHistory.size() != 0) {
-                this.bufferedWriter.write("Last " + chatHistory.size() + "messages : ");
-                this.bufferedWriter.newLine();
-                this.bufferedWriter.flush();
-
-                for (String message : chatHistory) {
-                    this.bufferedWriter.write("\t" + message);
-                    this.bufferedWriter.newLine();
-                    this.bufferedWriter.flush();
-                }
-            }
-        } catch (IOException e) {
-            closeEverything(socket , bufferedReader , bufferedWriter);
-
-        }
-    }
-    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
-        removeClientHandler();
-
-        try {
-            if (bufferedReader != null) {
-                bufferedReader.close();
-            }
-            if (bufferedWriter != null) {
-                bufferedWriter.close();
-            }
-            if (socket != null) {
-                socket.close();
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
     }
 }
