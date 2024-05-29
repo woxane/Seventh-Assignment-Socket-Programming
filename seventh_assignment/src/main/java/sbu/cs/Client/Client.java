3799e5ca25b7a4fce9abb81797911c85711c8457
woxane <mohsen.minavand1384@gmail.com>
2024-05-29 18:14:40 +0330

    Implement Client constructor to start file transfer

--- seventh_assignment/src/main/java/sbu/cs/Client/Client.java
+++ seventh_assignment/src/main/java/sbu/cs/Client/Client.java
@@ -13,84 +13,14 @@ public class Client {
     private static final int PORT = 12345;
 
 
-    public Client(Socket socket , String username) {
+    public Client(Socket socket) {
         try {
             this.socket = socket;
             this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            this.username = username;
 
         } catch (IOException e) {
             closeEverything(socket , bufferedReader , bufferedWriter);
         }
-
-    }
-
-
-    public void sendMessage() {
-        try {
-            bufferedWriter.write(this.username);
-            bufferedWriter.newLine();
-            bufferedWriter.flush();
-
-            Scanner scanner = new Scanner(System.in);
-            while (socket.isConnected()) {
-                String message = username + " : " + scanner.nextLine();
-                bufferedWriter.write(message);
-                bufferedWriter.newLine();
-                bufferedWriter.flush();
-            }
-
-        } catch (IOException e) {
-            closeEverything(socket , bufferedReader , bufferedWriter);
-        }
-    }
-
-
-    public void listenMessage() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                String message;
-
-                while (socket.isConnected()) {
-                    try {
-                        message = bufferedReader.readLine();
-                        System.out.println(message);
-                    } catch (IOException e) {
-                        closeEverything(socket , bufferedReader , bufferedWriter);
-                    }
-                }
-            }
-        }).start();
-    }
-
-    public void closeEverything(Socket socket , BufferedReader bufferedReader , BufferedWriter bufferedWriter) {
-        try {
-            if (socket != null) {
-                socket.close();
-            }
-            if (bufferedReader != null) {
-                bufferedReader.close();
-            }
-            if (bufferedWriter != null) {
-                bufferedWriter.close();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-
-    public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your username : ");
-        String username = scanner.nextLine();
-        Socket socket = new Socket("localhost" , PORT);
-        Client client = new Client(socket , username);
-        client.listenMessage();
-        client.sendMessage();
-
     }
 }
